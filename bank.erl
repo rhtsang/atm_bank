87213bd0f9a951c1be3cb7d2ba6c8fc7371cb0da
Raymond Tsang <rtsang1556@gmail.com>
2018-06-02 21:16:55 -0700

    additional bank API to handle ATMs

--- bank.erl
+++ bank.erl
@@ -3,7 +3,6 @@
 
 loop() ->
     receive
-
         % create a new bank @BankName and init with accounts in @AccountsList
         {create, AccountsList, BankName} ->
             ets:new(BankName, [named_table]),
@@ -22,7 +21,23 @@ loop() ->
         {balance, AccountName, BankName} ->
             Amount = ets:lookup(BankName, AccountName),
             io:format("account ~p has ~p dollars~n", [AccountName, Amount]),
-            loop()
+            loop();
 
+        % deposit @Amount dollars in account @AccountName in bank @BankName
+        {deposit, BankName, AccountName, Amount} ->
+            {_, CurrentAmount} = hd(ets:lookup(BankName, AccountName)),
+            ets:insert(BankName, CurrentAmount+Amount),
+            io:format("account ~p now has ~p dollars~n", [AccountName, CurrentAmount+Amount]),
+            loop();
+
+        % withdraw @Amount dollars from account @AccountName in bank @BankName
+        {withdraw, BankName, AccountName, Amount} ->
+            {_, CurrentAmount} = hd(ets:lookup(BankName, AccountName)),
+            case Amount > CurrentAmount of
+                false -> ets:insert(BankName, {AccountName, CurrentAmount-Amount}),
+                         io:format("account ~p now has ~p dollars~n", [AccountName, CurrentAmount-Amount]);
+                true -> io:format("sorry, account ~p has only ~p dollars~n", [AccountName, CurrentAmount])
+            end,
+            loop()
     end
 .
